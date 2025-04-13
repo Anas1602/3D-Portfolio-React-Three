e1a0b1b9c98a77c26b8d53ba2d1b9a5f8d12340e
Anas LARIBI <anas.laribi95@gmail.com>
2025-04-13 07:33:42 -0400

    Showcase Section with Animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "./sections/Hero";
-
+import Showcase from "./sections/Showcase";
 const App = () => {
   return (
     <>
       <Hero />
+      <Showcase />
     </>
   );
 };
